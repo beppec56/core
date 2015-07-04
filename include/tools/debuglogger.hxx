886deb3168fb149ebc9f5a8f8a4806d3fc4ee862
Giuseppe Castagno <giuseppe.castagno@acca-esse.eu>
2015-07-04 16:11:49 +0200

    Add a new logging component, debug only.
    The value DebugLoggerFileAgeInHours in Bootstrap section,
    in setuprc/setup.ini can be used to adapt the log file age
    in hours, for example:
    
    DebugLoggerFileAgeInHours=24
    
    means the file aged more than 24 hour from the LO startup time,
    will be cancelled.
    
    The default maximum file age is 5 days.
    
    Conflicts:
    	include/osl/diagnose.h
    
    Change-Id: If8eb0ff2558bb3b448da78234d1584de8544e6c4

---
+++ include/tools/debuglogger.hxx
@@ -0,0 +1,30 @@
+/* -*- Mode: C++; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*- */
+/*
+ * This file is part of the LibreOffice project.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+#ifndef INCLUDED_TOOLS_DEBUGLOGGER_HXX
+#define INCLUDED_TOOLS_DEBUGLOGGER_HXX
+
+#include <tools/toolsdllapi.h>
+
+#include <sal/detail/log.h>
+#include <sal/types.h>
+#include <rtl/ustring.hxx>
+
+namespace tools {
+    TOOLS_DLLPUBLIC void initDebugLog();
+    TOOLS_DLLPUBLIC void createAbsoluteLogFileSystemName(OUString & inFileNameRadix,  OUString & aSystemPathReturned, bool createAbsoluteLogFileSystemName );
+    TOOLS_DLLPUBLIC void deleteOlderLogFiles(int nFileAgeInHours, OUString & sFileNameRadix);
+    TOOLS_DLLPUBLIC void writeDebugLogDefaultName();
+    TOOLS_DLLPUBLIC void writeDebugLog(char const* fileName);
+    TOOLS_DLLPUBLIC void writeDebugLogDefaultNameIfNotEmpty();
+    TOOLS_DLLPUBLIC void writeDebugLogIfNotEmpty(char const* fileName);
+}
+
+
+#endif
