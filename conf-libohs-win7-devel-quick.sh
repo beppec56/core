b4bcf684298a5b20068f13c7a850c2fcc3dd4273
Giuseppe Castagno <giuseppe.castagno@acca-esse.eu>
2015-07-10 21:15:46 +0200

    [helpers] Added external tarballs directory
    Change-Id: Ia220d2c2d65c9d3442cf190a48397bf9c8c878ad

--- conf-libohs-win7-devel-quick.sh
+++ conf-libohs-win7-devel-quick.sh
@@ -6,6 +6,7 @@
 --with-vendor="Acca Esse, I-10067" \
 --with-gdrive-client-id="457862564325.apps.googleusercontent.com" \
 --with-gdrive-client-secret="GYWrDtzyZQZ0_g5YoBCC6F0I" \
+--with-external-tar=/srv5/git/LibO/externals \
 --with-lang='en-US' \
 \
 --with-external-tar=/cygdrive/e/sources/lo-externalsrc \
