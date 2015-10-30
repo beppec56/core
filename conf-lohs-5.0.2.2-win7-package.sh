9daea141fb87b7e2fe8ffcc476b34fd9e3a6b731
Giuseppe Castagno <giuseppe.castagno@acca-esse.eu>
2015-10-30 09:43:34 +0100

    Added helper script files to compile
    Change-Id: I0f361df42e8abbdc7ce392ef2eda4112550e81d2

---
+++ conf-lohs-5.0.2.2-win7-package.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+# configuration for windows
+
+. ../gdrive-lohs-credential.shinc
+
+./autogen.sh \
+--with-vendor="Acca Esse, I-10067" \
+--with-gdrive-client-id="$GDRIVE_CLIENT_ID" \
+--with-gdrive-client-secret="$GDRIVE_CLIENT_SECRET" \
+--with-lang='en-US it fr de pt-BR' \
+\
+--with-external-tar=/cygdrive/e/sources/lo-externalsrc \
+--with-junit=/cygdrive/c/sources/junit-4.10.jar \
+--with-ant-home=/cygdrive/c/sources/apache-ant-1.9.5 \
+--with-package-format=msi \
+--enable-pch --disable-ccache \
+--with-visual-studio=2013 \
+\
+--enable-extension-integration \
+--enable-scripting-beanshell \
+--enable-scripting-javascript \
+--enable-ext-wiki-publisher \
+--enable-ext-nlpsolver \
+\
+--with-help \
+--with-myspell-dicts \
+--disable-dependency-tracking \
+--enable-mergelibs \
+\
+--with-build-version="$(date +"%Y-%m-%d %H:%M:%S") - Rev. $(git branch |grep "*" | sed 's/* //g') $(echo git_$(git log -n1 --format=%h)) based on git_$(git log -n1 --format=%h libreoffice-5-0-2)" \
+
