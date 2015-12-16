598bb533dfbbc1c4f6a739fef01645e61380020c
Giuseppe Castagno <giuseppe.castagno@acca-esse.eu>
2015-12-16 10:43:56 +0100

    [helpers] Add some specific conf for lohs-5-1
    Change-Id: I605e477525f54d9c17bf7506f571f84a155f9ac6

--- conf-lohs-linux-14.04-hs-deb.sh
+++ conf-lohs-linux-14.04-hs-deb.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
 #get the branch point
-BRANCH_POINT=`diff -u <(git rev-list -200 --first-parent HEAD)   <(git rev-list -200 --first-parent libreoffice-5-0) | sed -ne 's/^ //p' | head -1`
-BRANCH_POINT2=`git log -n1 --format=%h $BRANCH_POINT`
+. ./conf-lohs-get-branch-point-lo51.sh
 
 . ../gdrive-lohs-credential.shinc
 
@@ -16,7 +15,6 @@ BRANCH_POINT2=`git log -n1 --format=%h $BRANCH_POINT`
 --with-system-zlib \
 --without-system-poppler \
 --without-system-openssl \
---without-system-mesa-headers \
 --without-system-libpng \
 --without-system-libxml \
 --without-system-jpeg \
@@ -30,10 +28,8 @@ BRANCH_POINT2=`git log -n1 --format=%h $BRANCH_POINT`
 --enable-dbus \
 --enable-extension-integration \
 --enable-odk \
---enable-lockdown \
 --enable-gstreamer-0-10 \
 --disable-gstreamer-1-0 \
---enable-gnome-vfs \
 \
 --enable-extension-integration \
 --enable-scripting-beanshell \
@@ -45,10 +41,10 @@ BRANCH_POINT2=`git log -n1 --format=%h $BRANCH_POINT`
 --disable-online-update  \
 --disable-gio \
 --disable-randr-link \
---enable-mergelibs \
 --with-package-format="deb" \
 --with-lang='en-US it fr de pt-BR' \
---with-build-version="$(date +"%Y-%m-%d %H:%M:%S") - Rev. $(git branch |grep "*" | sed 's/* //g')$(echo :$(git log -n1 --format=%h)) based on libreoffice-5-0:$BRANCH_POINT2" \
+--with-build-version="$(date +"%Y-%m-%d %H:%M:%S") - Rev. $(git branch |grep "*" | sed 's/* //g')$(echo :$(git log -n1 --format=%h)) based on $ROOT_BRANCH:$BRANCH_POINT2" \
 \
 
-make scp2.clean
+make scp2.clean > /dev/null 2>&1
+cat autogen.lastrun
