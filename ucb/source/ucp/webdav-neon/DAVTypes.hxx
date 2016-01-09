3d25c36f36c0188f0a02dac86000f4f035101880
Giuseppe Castagno <giuseppe.castagno@acca-esse.eu>
2016-01-09 17:32:26 +0100

    Add OPTIONS method 1: New class to store retrieved OPTIONS
    This class describes the DAV options useful to LO.
    Added behavioural unit tests as well.
    
    Change-Id: I7ba0230f1a1469b12f9b3f59f21e5c2730c65b81

--- ucb/source/ucp/webdav-neon/DAVTypes.hxx
+++ ucb/source/ucp/webdav-neon/DAVTypes.hxx
@@ -30,28 +30,117 @@
 #define INCLUDED_UCB_SOURCE_UCP_WEBDAV_NEON_DAVTYPES_HXX
 
 #include <config_lgpl.h>
+#include <memory>
+#include <list>
+#include <map>
+#include "osl/mutex.hxx"
+#include <rtl/uri.hxx>
 #include <rtl/ustring.hxx>
 #include <com/sun/star/uno/Any.hxx>
 
 namespace webdav_ucp
 {
+/* RFC 2518
 
-enum Depth { DAVZERO = 0, DAVONE = 1, DAVINFINITY = -1 };
+   15.1 Class 1
 
-enum ProppatchOperation { PROPSET = 0, PROPREMOVE = 1 };
+   A class 1 compliant resource MUST meet all "MUST" requirements in all
+   sections of this document.
 
-struct ProppatchValue
-{
-    ProppatchOperation  operation;
-    OUString            name;
-    css::uno::Any       value;
+   Class 1 compliant resources MUST return, at minimum, the value "1" in
+   the DAV header on all responses to the OPTIONS method.
 
-    ProppatchValue( const ProppatchOperation o,
-                    const OUString & n,
-                    const css::uno::Any & v )
-    : operation( o ), name( n ), value( v ) {}
-};
+   15.2 Class 2
 
+   A class 2 compliant resource MUST meet all class 1 requirements and
+   support the LOCK method, the supportedlock property, the
+   lockdiscovery property, the Time-Out response header and the Lock-
+   Token request header.  A class "2" compliant resource SHOULD also
+   support the Time-Out request header and the owner XML element.
+
+   Class 2 compliant resources MUST return, at minimum, the values "1"
+   and "2" in the DAV header on all responses to the OPTIONS method.
+*/
+
+    class DAVOptions
+    {
+    private:
+        bool    m_isResourceFound;   // true if the resource was found, else false
+        bool    m_isClass1;
+        bool    m_isClass2;
+        bool    m_isClass3;
+// contains the value of header ms-author-via
+// needed to detect if the server is Sharepoint-like
+        OUString    m_aAllowedMethods;
+
+        /// target time when this capability becomes stale
+        sal_uInt32 m_nStaleTime;
+        OUString  m_sURL;
+        OUString  m_sRedirectedURL;
+
+    public:
+        DAVOptions();
+
+        DAVOptions( const DAVOptions & rOther );
+
+        ~DAVOptions();
+
+        bool isResourceFound() { return m_isResourceFound; };
+        void setResourceFound( bool ResourceFound = true ) { m_isResourceFound = ResourceFound; };
+
+        bool isClass1() { return m_isClass1; };
+        void setClass1( bool Class1 = true ) { m_isClass1 = Class1; };
+
+        bool isClass2() { return m_isClass2; };
+        void setClass2( bool Class2 = true ) { m_isClass2 = Class2; };
+
+        bool isClass3() { return m_isClass3; };
+        void setClass3( bool Class3 = true ) { m_isClass3 = Class3; };
+
+        sal_uInt32  getStaleTime() { return m_nStaleTime ; };
+        void setStaleTime( sal_uInt32 nStaleTime ) { m_nStaleTime = nStaleTime; };
+
+        OUString & getURL() { return m_sURL; };
+        void setURL( OUString & sURL ) { m_sURL = sURL; };
+
+        OUString & getRedirectedURL() { return m_sRedirectedURL; };
+        void setRedirectedURL( OUString & sRedirectedURL ) { m_sRedirectedURL = sRedirectedURL; };
+
+        void  setAllowedMethods( OUString & aAllowedMethods ) { m_aAllowedMethods = aAllowedMethods; } ;
+        OUString & getAllowedMethods() { return m_aAllowedMethods; } ;
+        bool isLockAllowed() { return ( m_aAllowedMethods.indexOf( "LOCK" ) != -1 ); };
+        bool isUnlockAllowed() { return ( m_aAllowedMethods.indexOf( "UNLOCK" ) != -1 ); };
+
+        void reset() {
+            m_isResourceFound = false;
+            m_isClass1 = false;
+            m_isClass2 = false;
+            m_isClass3 = false;
+            m_aAllowedMethods.clear();
+            m_nStaleTime = 0;
+            m_sURL.clear();
+            m_sRedirectedURL.clear();
+        };
+
+        bool operator==( const DAVOptions& rOpts ) const;
+
+    };
+
+    enum Depth { DAVZERO = 0, DAVONE = 1, DAVINFINITY = -1 };
+
+    enum ProppatchOperation { PROPSET = 0, PROPREMOVE = 1 };
+
+    struct ProppatchValue
+    {
+        ProppatchOperation  operation;
+        OUString            name;
+        css::uno::Any       value;
+
+        ProppatchValue( const ProppatchOperation o,
+                        const OUString & n,
+                        const css::uno::Any & v )
+            : operation( o ), name( n ), value( v ) {}
+    };
 } // namespace webdav_ucp
 
 #endif // INCLUDED_UCB_SOURCE_UCP_WEBDAV_NEON_DAVTYPES_HXX
