4ab007fccc086546b13a7912eb610ea0ed477a25
Giuseppe Castagno <giuseppe.castagno@acca-esse.eu>
2016-01-11 17:44:47 +0100

    Add OPTIONS method 16: Add default Env to Content::getProperties
    Change-Id: I8dca1448d526234ecad615270dcb77a51722e580

--- ucb/source/ucp/webdav-neon/webdavcontentcaps.cxx
+++ ucb/source/ucp/webdav-neon/webdavcontentcaps.cxx
@@ -38,6 +38,7 @@
 #include <com/sun/star/beans/PropertyValue.hpp>
 #include <com/sun/star/ucb/CommandInfo.hpp>
 #include <com/sun/star/ucb/ContentInfo.hpp>
+#include <com/sun/star/ucb/CommandEnvironment.hpp>
 #include <com/sun/star/ucb/OpenCommandArgument2.hpp>
 #include <com/sun/star/ucb/InsertCommandArgument.hpp>
 #include <com/sun/star/ucb/PostCommandArgument2.hpp>
@@ -48,6 +49,7 @@
 #include <com/sun/star/ucb/Link.hpp>
 #include <com/sun/star/ucb/Lock.hpp>
 #include <com/sun/star/ucb/LockEntry.hpp>
+#include <com/sun/star/task/InteractionHandler.hpp>
 #include "webdavcontent.hxx"
 #include "webdavprovider.hxx"
 #include "DAVSession.hxx"
@@ -319,6 +321,20 @@ uno::Sequence< beans::Property > Content::getProperties(
     if ( !bTransient )
     {
         // Obtain all properties supported for this resource from server.
+        // xEnv is empty, use a default one, to use cached credential
+        css::uno::Reference< css::ucb::XCommandEnvironment > xAuthEnv;
+        if( !xEnv.is() )
+        {
+            css:: uno::Reference< task::XInteractionHandler > xIH(
+                css::task::InteractionHandler::createWithParent( m_xContext, 0 ), css::uno::UNO_QUERY_THROW );
+
+            xAuthEnv = css::ucb::CommandEnvironment::create(
+                m_xContext,
+                xIH,
+                css::uno::Reference< ucb::XProgressHandler >() ) ;
+        }
+        else
+            xAuthEnv = xEnv;
 
         getResourceOptions( xEnv );
         if ( m_aDAVCapabilities.isClass1() )
@@ -326,7 +342,7 @@ uno::Sequence< beans::Property > Content::getProperties(
             try
             {
                 std::vector< DAVResourceInfo > props;
-                xResAccess->PROPFIND( DAVZERO, props, xEnv );
+                xResAccess->PROPFIND( DAVZERO, props, xAuthEnv );
 
                 // Note: vector always contains exactly one resource info, because
                 //       we used a depth of DAVZERO for PROPFIND.
