898a9dbfa6f9d966218e34eec3d2370e9fb65dd7
Giuseppe Castagno <giuseppe.castagno@acca-esse.eu>
2016-03-15 17:33:29 +0100

    Added some debug log in libcmis, for gdrive 2fa study
    Change-Id: I6fecb3dab11602ebd816f91a48360fd91a664ff5

--- external/libcmis/UnpackedTarball_cmis.mk
+++ external/libcmis/UnpackedTarball_cmis.mk
@@ -15,6 +15,7 @@ $(eval $(call gb_UnpackedTarball_set_patchlevel,cmis,1))
 
 $(eval $(call gb_UnpackedTarball_add_patches,cmis, \
 						external/libcmis/libcmis-libxml2_compatibility.patch \
+						external/libcmis/cmis-log.patch \
 ))
 
 ifeq ($(OS)$(COM),WNTMSC)
